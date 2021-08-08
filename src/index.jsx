139b2d3b1b562e455f1b8fd25778b24e1bb6b283
vitornascimentto <vitornascimentto0gmail.com>
2021-08-08 16:10:40 -0300

    add redux, styles and web reports to the react application index

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+import store from './redux';
+
+import './styles/main.scss';
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
+
+render();
+
+reportWebVitals();
